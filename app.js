46e9c0493858c2a80a0e17849d5ffae98b3894d0
Artemii Pudovkin <tyumen-777@yandex.ru>
2022-02-28 01:23:11 +0500

    sprint_2: feat: выполнил задание для сдачи 2 проектной работы

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const express = require('express');
+const path = require('path')
 
 const app = express();
 const PORT = 3000 || process.env.PORT;
 
 app.use(express.static(`${__dirname}/dist`));
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(`${__dirname}/dist/index.html`))
+})
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
