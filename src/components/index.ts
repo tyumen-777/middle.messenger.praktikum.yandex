74edc78cdd9cf6a13d7b45f911f2e3f94ea676f5
artempudovkin <apudovkin@i-neti.ru>
2024-06-18 16:26:57 +0500

    feat: initial commit

---
+++ src/components/index.ts
@@ -0,0 +1,7 @@
+export * from './base-layout';
+export * from './form';
+export * from './field';
+export * from './button';
+export * from './button-icon';
+export * from './avatar'
+export * from './divider'
---
+++ src/main.ts
@@ -0,0 +1,45 @@
+import Handlebars from 'handlebars';
+import './styles/styles.scss';
+import * as Pages from './pages';
+import * as Components from './components';
+import { ArrowIcon, EmptyAvatar } from "./assets/icons";
+
+
+const pages: Record<string, any[]> = {
+  login: [Pages.LoginPage, {}],
+  signup: [Pages.SignupPage, {}],
+  chats: [Pages.ChatsPage],
+  profile: [Pages.ProfilePage, {
+    arrowIcon: ArrowIcon,
+    size: 'large',
+    emptyImage: EmptyAvatar
+  }],
+  '404': [Pages.ErrorPage, {error: '404', errorMessage: 'Не туда попали'}],
+  '500': [Pages.ErrorPage, {error: '500', errorMessage: 'Мы уже фиксим'}],
+};
+
+Object.entries(Components).forEach(([name, component]) =>
+  Handlebars.registerPartial(name, component),
+);
+
+const navigate = (page: keyof typeof pages) => {
+  const container = document.querySelector('#app');
+  const [source, context] = pages[page];
+
+  if (container) {
+    container.innerHTML = Handlebars.compile(source)(context);
+  }
+};
+
+document.addEventListener('DOMContentLoaded', () => navigate('profile'));
+
+document.addEventListener('click', (event) => {
+  const page = event.target as HTMLInputElement;
+  const attr = page.getAttribute('page');
+  if (attr) {
+    navigate(attr);
+
+    event.preventDefault();
+    event.stopImmediatePropagation();
+  }
+});
