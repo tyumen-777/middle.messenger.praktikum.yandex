a40d56c749d32dabd7c1131503fdc952a5c8e723
artempudovkin <apudovkin@i-neti.ru>
2024-10-07 15:27:57 +0500

    fix: исправить ошибки после тестов

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,7 +4,6 @@ module.exports = {
   parserOptions: {
     ecmaVersion: 2020,
     sourceType: 'module',
-    project: './tsconfig.json',
     tsconfigRootDir: __dirname,
   },
   ignorePatterns: ['dist', '.eslintrc.cjs'],
